dcfe1f13d1cfae81323ebd5827b85f1852b4b439
Khoa Diệp <143460572+Misax10@users.noreply.github.com>
2024-04-27 13:13:04 -0700

    Set up the timezone offset

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -5,9 +5,12 @@ const ChatsPage = (props) => {
         '4d3b837f-f00d-4d27-9389-2cf7cc2574d7', 
         props.user.username, 
         props.user.secret)
+    // Set timezoneOffset to -7 for Pacific Daylight Time
+    const timezoneOffset = -7;
+    
     return( 
     <div style={{height:'100vh'}}>
-        <MultiChatSocket {...chatProps}/>
+        <MultiChatSocket {...chatProps} timezoneOffset={timezoneOffset}/>
         <MultiChatWindow {...chatProps} style={{height:'100%'}}/>
     </div>
     )
